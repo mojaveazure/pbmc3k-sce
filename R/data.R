c7090ab4fae7cef627edad8dc136706712d57e2f
Paul Hoffman <paul.hoffman@tiledb.com>
2023-08-05 21:46:44 -0400

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' pbmc3k
+#'
+#' @format A \sce object with the pbmc3k dataset
+#'
+#' @source \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k}
+#'
+"pbmc3k"
+
+#' pbmc3k.final
+#'
+#' @format A \sce object with the pbmc3k.final dataset
+#'
+#' @source \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets/1.1.0/pbmc3k}
+#' @source \url{https://satijalab.org/seurat/v3.0/pbmc3k_tutorial.html}
+#' @source \url{http://bioconductor.org/books/3.17/OSCA.intro/analysis-overview.html}
+#'
+"pbmc3k.final"
